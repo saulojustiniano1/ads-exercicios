e2c42eb62c2893aef30bd63a0543dc33b4ace558
saulojustiniano1 <saulojustiniano1@gmail.com>
2023-04-13 12:09:03 -0300

    📦create: basicos-1

---
+++ basicos-1/1006_media_2.py
@@ -0,0 +1,7 @@
+nota_A = float(input())
+nota_B = float(input())
+nota_C = float(input())
+
+media = (nota_A * 2 + nota_B * 3 + nota_C * 5) / (2 + 3 + 5)
+
+print(f'MEDIA = {media:.1f}')
---
+++ basicos-1/1007_diferenca.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+diferenca_do_produto = (a * b) - (c * d)
+
+print(f'DIFERENCA = {diferenca_do_produto}')
