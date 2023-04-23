8744174cd092c39c2340735c06578103dc509337
saulojustiniano1 <saulojustiniano1@gmail.com>
2023-04-23 15:46:22 -0300

    📤update: sequencia-IJ-4

--- beecrowd/repeticao-while/1098_sequencia-IJ-4.py
+++ beecrowd/repeticao-while/1098_sequencia-IJ-4.py
@@ -1,17 +1,24 @@
 i = 0
 j = 1
+c = 0
 aux_i = i
 aux_j = j
 
 while i <= 2 and j <= 5:
-  print(f'I={round(i, 1)} J={round(j, 1)}')
-  j += 1
+  while c < 3:
+    if i in [0, 1, 1.8]:
+      print(f'I={i:.0f} J={j:.0f}')
+    else:
+      print(f'I={i:.1f} J={j:.1f}')
+    c += 1
+    j += 1
 
   if j >= 4:
+    c = 0
     i = aux_i
     j = aux_j
     i += 0.2
     j += 0.2
     aux_i = i
     aux_j = j
-
+  
