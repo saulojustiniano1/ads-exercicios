e95ef093cb1102e353659856acdecff25faeaa86
saulojustiniano1 <saulojustiniano1@gmail.com>
2023-05-29 13:22:20 -0300

    📤update: deleted

--- beecrowd/funcoes(def)/def_basicos-1/1003_soma-simples.py
+++
@@ -1,9 +0,0 @@
-a = int(input())
-b = int(input())
-
-def calc_soma(a, b):
-  return a + b
-
-soma = calc_soma(a, b)
-
-print(f'SOMA = {soma}')
--- beecrowd/funcoes(def)/def_basicos-1/1004_prod-simples.py
+++
@@ -1,9 +0,0 @@
-a = int(input())
-b = int(input())
-
-def calc_prod(a, b):
-  return a * b
-
-prod = calc_prod(a, b)
-
-print(f'PROD = {prod}')
