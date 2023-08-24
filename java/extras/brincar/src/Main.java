fc44892fec9aaee78f826972cf6307347db99cc2
saulojustiniano1 <saulojustiniano1@gmail.com>
2023-08-23 21:32:32 -0300

    📦create: testes da linguagem java (aprendizagem)

---
+++ java/extras/brincar/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Object x = 9;
+
+        Integer valor = 10;
+        String nome;
+
+        System.out.println(valor);
+
+        nome = valor.toString();
+
+        System.out.println(nome);
+
+        boolean test = x.equals(valor);
+
+        System.out.println(test);
+
+//        System.out.println(x+valor);
+
+
+
+    }
+}
